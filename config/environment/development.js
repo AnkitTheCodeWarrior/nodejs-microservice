2c94292eb092694d0f66d5ae222e808d91c5f0d0
ankitkumar <ankit@transneuron.com>
2020-03-05 13:19:25 +0530

    removed unwanted function

--- config/environment/development.js
+++ config/environment/development.js
@@ -6,7 +6,7 @@ module.exports = {
     isProd: false
   },
   database: {
-    uri: process.env.uri || 'mongodb://10.0.1.198:27017,10.0.2.182:27017,10.0.2.114:27017/SDMSUat?replicaSet=SDMSUat',
+    uri: process.env.uri || '',
     auth: process.env.auth || { user: 'testuser', password: 'testuser123' },
     withoutAuth: 'mongodb://localhost:27017/loginProject',
     isProd: false
@@ -17,13 +17,9 @@ module.exports = {
   },
   externalServices: {
     authService: 'http://localhost:3001/api/auth',
-    trancheService: "http://localhost:1044/api/v1/tranche/blocklisttranche",
-    emailService : "https://sdmsservices.nsdcindia.org/api/v1/comm/node/email",
-    userService : "http://localhost:3000/api/user/v1/",
-    userServiceBatch: "http://localhost:3000/api/batch/v1/pmkvy/"
   },
   securityDetails: {
-    secretKey: 'transneuron',
+    secretKey: '%$WYCYHG^%HUI*',
     whitelistIPs: []
   },
   controls: {
